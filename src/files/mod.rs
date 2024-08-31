03ec858e7df81794bad36791a424a644788946f8
gabokatta <gkatta@fi.uba.ar>
2024-08-30 23:31:18 -0300

    slight refactor to string formatting in errored

--- src/files/mod.rs
+++ src/files/mod.rs
@@ -5,17 +5,17 @@ pub fn validate_path(dir: &str) -> Result<(), Errored> {
     let path = Path::new(dir);
 
     if !path.exists() {
-        return Err(Errored(format!("path {} does not exist", dir)));
+        return Err(Errored(format!("path '{dir}' does not exist")));
     }
     if !path.is_dir() {
-        return Err(Errored(format!("path {} is not a valid directory", dir)));
+        return Err(Errored(format!("path '{dir}' is not a valid directory")));
     }
 
     let dir_entries = path
         .read_dir()
-        .map_err(|e| Errored(format!("failure when reading directory {}: {}", dir, e)))?;
+        .map_err(|e| Errored(format!("failure when reading directory '{dir}': {e}")))?;
     if dir_entries.count() == 0 {
-        return Err(Errored(format!("path {} is an empty directory", dir)));
+        return Err(Errored(format!("path '{dir}' is an empty directory")));
     }
     Ok(())
 }
