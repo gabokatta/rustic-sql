52ac61691dc461f873a9693769c1789a1694cbc3
gabokatta <gkatta@fi.uba.ar>
2024-09-09 11:17:27 -0300

    final doc update

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,40 @@
+//! # RusticSQL 🦀
+//!
+//! ### Clon de SQL, en Rust.
+//!
+//! Permite al usuario ejecutar consultas SQL mediante la linea de comandos.
+//!
+//! Las operaciones se realizan sobre "tablas" (archivos csv).
+//!
+//!
+//! Consultas Permitidas: [SELECT, INSERT, UPDATE, DELETE]
+//!
+//! Operadores Disponibles: [AND, OR, NOT y comparadores simples (>, <, =, etc..)]
+//!
+//!
+//! Estructura del Proyecto:
+//! - Tokenizador: Recibe un String y te devuelve tokens.
+//! - Constructor: Recibe tokens y los transforma en consultas validas.
+//! - Ejecutor: Recibe consultas y las ejecuta sobre las tablas.
+//!
+//! # Usar RusticSQL:
+//!
+//! > ```BASH
+//! > cargo run -- ruta/a/tablas "SELECT * FROM table" > output.csv
+//! > ```
+//!
+//! # Testea RusticSQL:
+//!
+//! >```BASH
+//! >cargo test --all
+//! >```
+//!
+//!
+#![doc(
+    html_logo_url = "https://cdn-icons-png.flaticon.com/512/4726/4726022.png",
+    html_favicon_url = "https://cdn-icons-png.flaticon.com/512/4726/4726022.png"
+)]
+
 use crate::query::executor::Executor;
 use crate::query::structs::query::Query;
 use crate::query::tokenizer::Tokenizer;
@@ -8,22 +45,6 @@ use std::error::Error;
 pub mod query;
 pub mod utils;
 
-/// Ejecuta la aplicación RusticSQL a partir de los argumentos proporcionados.
-///
-/// Esta función valida los argumentos de la línea de comandos, procesa la consulta SQL y ejecuta
-/// la consulta en los datos especificados.
-///
-/// # Argumentos
-///
-/// - `args`: Un vector de `String` que contiene los argumentos de la línea de comandos. Se espera que
-///   contenga exactamente tres elementos: el nombre del comando, la ruta a las tablas y la consulta SQL.
-///
-/// # Errores
-///
-/// - Retorna un error si el número de argumentos es incorrecto.
-/// - Retorna un error si la ruta a las tablas no es válida.
-/// - Retorna un error si la consulta SQL no es válida.
-/// - Retorna un error si el procesamiento de la consulta o la ejecución falla.
 pub fn run(args: Vec<String>) -> Result<(), Box<dyn Error>> {
     if args.len() != 3 {
         println!("invalid usage of rustic-sql");
